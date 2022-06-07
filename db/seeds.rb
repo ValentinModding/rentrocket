d9f23c67437a87ff7f0555db2b49bccf882f6d7d
Clemence Dumont <cdumont90@gmail.com>
2022-06-07 15:38:09 +0200

    Users et voitures ajoutés dans les seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,30 @@ user1 = User.create(
   password_confirmation: "totototo"
 )
 
+user2 = User.create(
+  email: "valentin@gmail.com",
+  password: "valentinvalentin",
+  password_confirmation: "valentinvalentin"
+)
+
+user3 = User.create(
+  email: "clem@gmail.com",
+  password: "clemclem",
+  password_confirmation: "clemclem"
+)
+
+user4 = User.create(
+  email: "mathilde@gmail.com",
+  password: "mathildemathilde",
+  password_confirmation: "mathildemathilde"
+)
+
+user5 = User.create(
+  email: "julien@gmail.com",
+  password: "julienjulien",
+  password_confirmation: "julienjulien"
+)
+
 Offer.create(
   title: "Ferrari 488GTB",
   price: "300",
@@ -23,4 +47,48 @@ Offer.create(
   km_age: "1500",
   body_type: "3",
   user: user1
+)
+
+Offer.create(
+  title: "Loue une belle Lambo",
+  price: "450",
+  brand: "Lamborghini",
+  model: "HURACÁN EVO RWD",
+  horsepower: "500",
+  km_age: "1000",
+  body_type: "2",
+  user: user2
+)
+
+Offer.create(
+  title: "Loue une belle Porsche",
+  price: "350",
+  brand: "Porsche",
+  model: "HTAYCAN 4S",
+  horsepower: "435",
+  km_age: "1800",
+  body_type: "2",
+  user: user3
+)
+
+Offer.create(
+  title: "Loue une belle Bentley",
+  price: "480",
+  brand: "Bentley",
+  model: "CONTINENTAL GT CABRIOLET",
+  horsepower: "630",
+  km_age: "1200",
+  body_type: "3",
+  user: user4
+)
+
+Offer.create(
+  title: "Loue une belle Porsche bleue!",
+  price: "600",
+  brand: "Porsche",
+  model: "CAYMAN GT4",
+  horsepower: "420",
+  km_age: "800",
+  body_type: "2",
+  user: user5
 )
